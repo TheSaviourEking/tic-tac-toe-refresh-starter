bf7cb9bd2196793a96b1962af412f21465136c4d
Saviour Eking <saviour.eking@outlook.com>
2023-09-19 00:03:56 +0100

    Update cursor.js

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -6,60 +6,60 @@ class Cursor {
     this.numRows = numRows;
     this.numCols = numCols;
 
-    this.row = 0;
-    this.col = 0;
+    this.row = 0; // 0
+    this.col = 0; // 0
 
     this.gridColor = 'black';
     this.cursorColor = 'yellow';
-
   }
 
   resetBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.gridColor);
-    Screen.render();
   }
 
   setBackgroundColor() {
-    Screen.initialize(this.numRows, this.numCols);
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
+  }
+
+  #movementHelper = (directions) => {
+    Screen.render();
+    this.resetBackgroundColor();
+    directions == "up" ? this.row--
+      : directions == "down" ? this.row++
+        : directions == "left" ? this.col--
+          : this.col++;
+    this.setBackgroundColor();
     Screen.render();
   }
 
-  up() {
+  up = () => {
     // Move cursor up
-    if (this.row > 0 && this.row < this.numRows - 1) this.row--
+    if (this.row > 0) {
+      this.#movementHelper('up')
+    }
   }
 
-  down() {
+  down = () => {
     // Move cursor down
-    // this.row++;
-    if (this.row >= 0 && this.row < this.numRows - 1) this.row++
+    if (this.row < this.numRows - 1) {
+      this.#movementHelper('down')
+    }
   }
 
-  left() {
+  left = () => {
     // Move cursor left
-    if (this.col > 0 && this.col < this.numCols - 1) this.col--
+    if (this.col > 0) {
+      this.#movementHelper('left')
+    }
   }
 
-  right() {
+  right = () => {
     // Move cursor right
-    if (this.col < this.numCols - 1) this.col++
+    if (this.col < this.numCols - 1) {
+      this.#movementHelper('right');
+    }
   }
 
 }
-
-// let cursor = new Cursor(3, 3);
-// console.log(Screen)
-// cursor.setBackgroundColor()
-// cursor.resetBackgroundColor()
-// console.log(cursor)
-// cursor.down();
-// cursor.down();
-// cursor.down();
-// cursor.up();
-// cursor.down();
-// cursor.up()
-// cursor.right(); cursor.right(); cursor.right()
-// console.log(cursor)
 
 module.exports = Cursor;
